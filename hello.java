512e620290b2ce559aecf7845b14e38865a6d04c
Steven Brazil <stvnbrzl@gmail.com>
2020-10-04 00:52:31 -0500

    Update hello.java
    Updated program to include if/else statement

--- hello.java
+++ hello.java
@@ -1,11 +1,23 @@
 public class hello {
-    public static void main(String[] args){
-        System.out.println("Hello Steve");
+    public static void main(String[] args) {
 
-        int myFirstNumber = 28;
-        String name = "Steve";
+        int age = 29;
+        String name = "Rob";
 
-        System.out.println(name + " is " + myFirstNumber);
+        if(name == "Steve" && age == 28)
+        {
+            System.out.println("Hello, " + name + ". You are, " + age);
+        }
+
+        else if(name == "Steve" && age >= 29)
+        {
+            System.out.println("Woah " + name + ", you are older from when i last spoke to you!");
+        }
+
+        else
+        {
+            System.out.println("Please validate program to include your information. Thank you!");
+        }
 
     }
 }
