0aa0b794ad539d38981d536f9656e4b6dcc969ac
Steven Brazil <stvnbrzl@gmail.com>
2020-10-04 00:17:13 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello Steve");
+
+        int myFirstNumber = 28;
+        String name = "Steve";
+
+        System.out.println(name + " is " + myFirstNumber);
+
+    }
+}
